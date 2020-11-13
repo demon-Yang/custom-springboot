9b06816454c6bbd1ebc07e5b1fa332797574eef7
demon-yang <457890978@qq.com>
2020-11-13 17:41:54 +0800

    'kryo和lomback工具类'

---
+++ src/main/java/com/yxd/util/KryoUtil.java
@@ -0,0 +1,45 @@
+package com.yxd.util;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @Description：kryo序列化工具类
+ * @Date 2020/11/13 15:40
+ * @Author YXD
+ * @Version 1.0
+ */
+public class KryoUtil {
+    public static byte[] serialize(Object obj) {
+        Kryo kryo = kryoLocal.get();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream);
+        kryo.writeClassAndObject(output, obj);
+        output.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static <T> T deserialize(byte[] bytes) {
+        Kryo kryo = kryoLocal.get();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        Input input = new Input(byteArrayInputStream);
+        input.close();
+        return (T) kryo.readClassAndObject(input);
+    }
+
+    private static final ThreadLocal<Kryo> kryoLocal = new ThreadLocal<Kryo>() {
+        @Override
+        protected Kryo initialValue() {
+            Kryo kryo = new Kryo();
+            //默认值为true,强调作用
+            kryo.setReferences(true);
+            //默认值为false,强调作用
+            kryo.setRegistrationRequired(false);
+            return kryo;
+        }
+    };
+}
---
+++ src/test/java/com/yxd/util/KryoUtilTest.java
@@ -0,0 +1,38 @@
+package com.yxd.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description：
+ * @Date 2020/11/13 16:20
+ * @Author YXD
+ * @Version 1.0
+ */
+class KryoUtilTest {
+
+    @BeforeEach
+    void setUp() {
+        LogbackUtil.info("--- kryo序列化开始 ---");
+    }
+
+    @AfterEach
+    void tearDown() {
+        LogbackUtil.info("--- kryo序列化结束 ---");
+    }
+
+    @Test
+    void serialize() {
+        String testString = "hello world!";
+        byte[] bytes = KryoUtil.serialize(testString);
+        LogbackUtil.info("--- kryo序列化结果 {} ---", bytes);
+    }
+
+    @Test
+    void deserialize() {
+        byte[] bytes = new byte[]{3, 1, 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, -95};
+        Object deserialize = KryoUtil.deserialize(bytes);
+        LogbackUtil.info("--- kryo反序列化结果 {} ---", deserialize);
+    }
+}
