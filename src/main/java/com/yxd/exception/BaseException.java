da5de19e2fcc0da0cdaa6c2321da3d2619735497
demon-yang <457890978@qq.com>
2020-11-17 22:07:11 +0800

    '添加异常处理'

---
+++ src/main/java/com/yxd/exception/BaseException.java
@@ -0,0 +1,16 @@
+package com.yxd.exception;
+
+import com.yxd.util.LogbackUtil;
+
+/**
+ * @Description：自定义异常类
+ * @Date 2020/11/13 15:40
+ * @Author YXD
+ * @Version 1.0
+ */
+public class BaseException extends Exception {
+    public BaseException(String message) {
+        super(message);
+        LogbackUtil.error(message);
+    }
+}
---
+++ src/test/java/com/yxd/exception/BaseExceptionTest.java
@@ -0,0 +1,16 @@
+package com.yxd.exception;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description：异常类测试
+ * @Date 2020/11/17 20:28
+ * @Author YXD
+ * @Version 1.0
+ */
+class BaseExceptionTest {
+    @Test
+    void testException() throws BaseException{
+        throw new BaseException("测试基础错误");
+    }
+}
