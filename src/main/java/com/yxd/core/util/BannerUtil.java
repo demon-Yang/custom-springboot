dc186cc0a701ee75a80281139ad40729e87b83c2
demon-yang <457890978@qq.com>
2020-11-19 23:25:22 +0800

    'banner读取'

---
+++ src/main/java/com/yxd/core/util/BannerUtil.java
@@ -0,0 +1,26 @@
+package com.yxd.core.util;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @Description：banner读取
+ * @Date 2020/11/19 23:01
+ * @Author YXD
+ * @Version 1.0
+ */
+public class BannerUtil {
+    private static final String BANNERURL = "banner.txt";
+
+    public static void printBanner() {
+        try {
+            URL url = Thread.currentThread().getContextClassLoader().getResource(BANNERURL);
+            Path path = Paths.get(url.toURI());
+            Files.lines(path).forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/yxd/core/util/BannerUtilTest.java
@@ -0,0 +1,19 @@
+package com.yxd.core.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Description：
+ * @Date 2020/11/19 23:23
+ * @Author YXD
+ * @Version 1.0
+ */
+class BannerUtilTest {
+
+    @Test
+    void printBanner() {
+        BannerUtil.printBanner();
+    }
+}
